3ad10606f4d595718079c968f154ec07b90c641a
Kevin Smith <keveightysev@gmail.com>
2019-08-25 20:51:43 -0500

    Defined binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -16,8 +16,15 @@ def binary_search(arr, target):
   low = 0
   high = len(arr)-1
   
-  mid = len(arr) // 2
-  print(mid)
+  while low <= high:
+    mid = (low + high) // 2
+
+    if target < arr[mid]:
+      high = mid - 1
+    elif target > arr[mid]:
+      low = mid + 1
+    else:
+      return mid
 
   return -1 # not found
 
